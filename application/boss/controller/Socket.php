dda37c157d5b6681948f2f135bd860450ec25bd9
songjianan <song_121292057@126.com>
2019-09-16 15:25:24 +0800

    'socket'

--- application/boss/controller/Socket.php
+++ application/boss/controller/Socket.php
@@ -8,14 +8,23 @@ use think\swoole\Server;
 
 class Socket extends Server
 {
+    protected $host = '127.0.0.1';
+    protected $port = 9502;
+    protected $option = [
+        'worker_num'=> 4,
+        'daemonize'	=> true,
+        'backlog'	=> 128
+    ];
+
+
     /**
      * 显示资源列表
      *
      * @return \think\Response
      */
-    public function onReceive()
+    public function onReceive($server, $fd, $from_id, $data)
     {
-        //
+        $server->send($fd, 'Swoole: '.$data);
     }
 
 
